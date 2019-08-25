ab25a4b0dc414176d3df43e0778c28b1bab55934
RodrigoGelacio <rodrigopottertenis@live.com.mx>
2019-08-25 12:21:31 -0500

    barra arreglada

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -21,37 +21,37 @@ export class Tab3Page {
         data: {
             labels: ['Dia 1','Dia 2','Dia 3','Dia 4','Dia 5','Dia 6','Dia 7'],
             datasets: [{
-                label: 'Threshold', 
-                
+                label: 'Healthy',
+              
+                data: [98,96,96,98,99,97,0],
+                backgroundColor: 
 
-                data: [98,96,96,98,99,97,96],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                    'rgba(100, 50, 250, 0.2)',
+                
+                
                 borderWidth: 1
-            }]
+            },
+            {
+              label: 'Hyperthyroidism',
+              data: [0,0,0,0,0,0,97],
+              backgroundColor: 
+                  'rgba(255, 99, 132, 0.2)',
+
+              
+              
+              borderWidth: 1
+          }]
         },
         options: {
             scales: {
+              xAxes: [{stacked:true}],
+
+              
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
                     }
-                }]
+                },{stacked:true}]
             }
         }
     });
